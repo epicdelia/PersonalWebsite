8c69f44251ac6019cf00ffed179beb2c9e6bbd0e
epicdelia <epicdelia@gmail.com>
2019-01-14 10:47:45 -0500

    Added create, view, update and delete for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-resources :todo_lists do
-   resources :todo_items
-  end
-root "todo_lists#index"
+	resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_165403) do
+ActiveRecord::Schema.define(version: 2019_01_12_234027) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
