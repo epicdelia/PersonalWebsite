d40b98018e127cd0e19df1c2a264046d25949591
epicdelia <epicdelia@gmail.com>
2019-02-15 19:05:07 -0500

    Added template for adding new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,23 @@
 class ProjectsController < ApplicationController
 	def index
-		@posts = Post.all.order("created_at desc")
+		@projects = Projects.all.order("created_at desc")
 	end
+	def new
+		@project = Project.new
+	end
+	def create
+		@project = Project.new project_params 
+
+		if @project.save
+			redirect_to @project, notice: "Successfully saved the Project"
+		else
+			render 'new'
+		end 
+	end 
+
+	private 
+
+	def project_params 
+		params.require(:project).permit(:title, :description, :link)
+	end 
 end
